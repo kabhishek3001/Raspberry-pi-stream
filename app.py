d5847e0c58c968f4adf438caa1d2fd587082b771
kabhishek3001 <145182949+kabhishek3001@users.noreply.github.com>
2025-02-12 09:33:58 +0530

    app.py

--- app.py
+++ app.py
@@ -13,9 +13,12 @@ basic_auth = BasicAuth(app)
 last_epoch = 0
 
 picam2 = Picamera2()
-config = picam2.create_video_configuration(main={"format": "RGB888", "size": (640, 480)})
+config = picam2.create_video_configuration(
+    main={"format": "RGB888", "size": (640, 480)}
+)
 picam2.configure(config)
 picam2.start()
+
 
 def generate_frames():
     while True:
@@ -31,15 +34,22 @@ def generate_frames():
             frame_bytes = buffer.tobytes()
 
             # Yield frame as part of HTTP response
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+            yield (
+                b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n'
+            )
         else:
             print("No frame available!")
         time.sleep(0.1)  # Simulate a frame rate
 
+
 @app.route('/video_feed')
 def video_feed():
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(
+        generate_frames(),
+        mimetype='multipart/x-mixed-replace; boundary=frame'
+    )
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
